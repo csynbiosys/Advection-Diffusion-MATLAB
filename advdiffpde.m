18088f36484129318a84476ef581672eb346924a
fmenol <filippo.menolascina@ed.ac.uk>
2016-05-22 17:24:44 +0100

    Sin D with Initial half step IC
    This code simulates a an advection diffusion equation with an initial
    condition being 1 for x<=0.5 and 0 otherwise.
    I also modified the vidAD function to plot the normalised D instead of
    the concentration gradient.
    
    Run this code:
    advdiff(0.01,1);vidAD(0.01,1)

--- advdiffpde.m
+++ advdiffpde.m
@@ -7,5 +7,5 @@ Ka=pars(1); %change Ka
 Vc=DuDx(2)/((Ki+C)*(Ka+C));
 
 c = [1; 1];
-f = [((sin(2*pi*2.5.*x)+1)./2)/10;1].*DuDx;%+[-Vc; 0].*u; % changed DC DB =1;
+f = [((sin(2*pi*2.5.*x)+1)./2)/(10^-1);1].*DuDx;%+[-Vc; 0].*u; % changed DC DB =1;
 s = [0;0]; 
--- vidAD.m
+++ vidAD.m
@@ -2,7 +2,7 @@ function showAD(ka,R)
 load(strcat('logsensing_Ka=',num2str(ka,'%.5f'),'_R=',num2str(R,'%.5f'),'.mat'))
     for i=1:length(t)
         figure(gcf);
-        plot(x,sol(i,:,2),x,sol(i,:,1));
+        plot(x,((sin(2*pi*2.5.*x)+1)./2)/(10^-1)./(max(((sin(2*pi*2.5.*x)+1)./2)/(10^-1))),x,sol(i,:,1));
         axis([0 1 0 max(max(sol(:,:,1)))*1.1]);
         title(num2str(t(i),'%1.3f'));
         pause(0.03);
